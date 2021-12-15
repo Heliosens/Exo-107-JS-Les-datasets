09779aa1ceb6de6f37af1d204f481bd837f704b6
Sylvie-B <unsolo@hotmail.fr>
2021-12-15 23:54:54 +0100

    work on data set

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 // Elements pour lesquels vous créerez des datasets directement dans le code HTML.
 let p1 = document.getElementById('p1');
-let p2 = document.getElementById('p1');
-let p3 = document.getElementById('p1');
-let p4 = document.getElementById('p1');
+let p2 = document.getElementById('p2');
+let p3 = document.getElementById('p3');
+let p4 = document.getElementById('p4');
 
 // Elements pour lesquels vous créerez des datasets depuis JavaScript.
-let p5 = document.getElementById('p1');
-let p6 = document.getElementById('p1');
-let p7 = document.getElementById('p1');
-let p8 = document.getElementById('p1');
+let p5 = document.getElementById('p5');
+let p6 = document.getElementById('p6');
+let p7 = document.getElementById('p7');
+let p8 = document.getElementById('p8');
 
+p5.dataset.number = "10";
+p6.dataset.text = "nouveau texte";
+p7.dataset.array = "one|two|three";
+p8.dataset.test = "true";
