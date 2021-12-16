6ffacee3db01dd374175a7b1624f398d86653482
Sylvie-B <unsolo@hotmail.fr>
2021-12-16 10:42:16 +0100

    data set

--- app.js
+++ app.js
@@ -10,7 +10,29 @@ let p6 = document.getElementById('p6');
 let p7 = document.getElementById('p7');
 let p8 = document.getElementById('p8');
 
-p5.dataset.number = "10";
+p5.dataset.number = "15";
 p6.dataset.text = "nouveau texte";
 p7.dataset.array = "one|two|three";
 p8.dataset.test = "true";
+
+p1.dataset.number = (parseInt(p1.dataset.number) + 10).toString();
+p2.dataset.text = (p2.dataset.text.length).toString();
+
+let arr3 =  p3.dataset.array.split("|");
+for(let line of arr3){
+    p3.innerHTML += "<br>" + line;
+}
+
+let val = p4.dataset.test;
+p4.innerHTML = val === true ? "La valeur est vraie" : "La valeur est fause";
+
+let newP = document.createElement("p");
+
+document.body.appendChild(newP);
+
+let Person = {
+    name : "Doe",
+    surname : "John"
+}
+
+newP.dataset.perso = JSON.stringify(Person);
